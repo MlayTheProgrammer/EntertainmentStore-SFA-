5308303cb0ff5e5327d39faac6564e7b1331800e
Carl Hawley <hawleyc@jacks.sfasu.edu>
2021-10-25 17:01:29 -0500

    commenting customer

--- Customer.cs
+++ Customer.cs
@@ -5,53 +5,64 @@ namespace Homework1_Entertainment_Store
 {
     public class Customer : ICustomer
     {
-        private int MAX_RENTALS = 3;
-        private string name;
-        private ICustomerBehavior behavior;
-        private List<IRental> rentals;
-        private int numGamesToRent;
-        private int numNightsToRent;
+        //=======FIELDS=======
+        private int MAX_RENTALS = 3;        // maximum number of rentals a customer can have
+        private string name;                // customer's first name
+        private ICustomerBehavior behavior; // renting startegy of a customer
+        private List<IRental> rentals;      // list of rentals tha a customer currently has
+        private int numGamesToRent;         // the number of games a customer will rent if given the opportunity.
+        private int numNightsToRent;        // the number of nights a customer will rent games for if given the oppotunity.
 
+        //=======CTORS========
         public Customer(string name, ICustomerBehavior behavior)
         {
             this.name = name;
             this.behavior = behavior;
             this.rentals = new List<IRental>();
             RollBehavior();
-          //  Console.WriteLine("Customer<" + this.name + ", " + this.behavior.GetType().Name + ">"); 
         }
+
+        //=======METHODS======
         public string GetName()
         {
+            // Returns the first name of the customer.
+
             return this.name;
+        }
+        public bool CanRent(int storeStock)
+        {
+            // Returns whether or not the customer is allowed to rent games:
+            // The number of games that a customer can rent must be less than or equal to the given number of games a store has in stock.
+            // The number of games the customer wants to rent must be less than or equal to the given number of games a store has in stock.
+            // The number of games a customer wants to rent must be less than or equal to the number of games a customer can rent.
+
+            return ((MAX_RENTALS - numGamesRented()) <= storeStock) && (this.numGamesToRent <= storeStock) && ((MAX_RENTALS - numGamesRented()) >= this.numGamesToRent);
         }
         public void RollBehavior()
         {
+            // Generates the number of games and nights that a customer will rent, and assigns them.
+
             this.numGamesToRent = this.behavior.GenNumGamesToRent();
             this.numNightsToRent = this.behavior.GenNumNightsToRent();
         }
-        private int numGamesRented()
-        {
-            int count = 0;
-            foreach (IRental r in this.rentals)
-            {
-                foreach (IGame g in r.GetGames())
-                {
-                    count++;
-                }
-            }
-            return count;
-        }
+
         public IRental CreateRental(int day)
         {
+            // Generates and returns a new rental request for the given day, to be populated with games by a store.
+
             return new Rental(day, this.numGamesToRent, this.numNightsToRent, this.name);
         }
         public double PayFor(IRental r)
         {
+            // Adds the given rental to the list of rentals that the customer has, then returns the cost (paying for it).
+
             this.rentals.Add(r);
             return r.GetTotalCost();
         }
         public List<IRental> ReturnGames(int day)
         {
+            // Iterates through all rentals and removes and adds to a list any that are due on the given day, then returns them.
+
             List<IRental> returns = new List<IRental>();
             foreach (IRental r in this.rentals)
             {
@@ -71,9 +82,22 @@ namespace Homework1_Entertainment_Store
         {
             return "Customer<" + this.name + ", "+this.behavior.GetType().Name+">";
         }
-        public bool CanRent(int storeStock)
+
+        //=======HELPERS======
+        private int numGamesRented()
         {
-            return ((MAX_RENTALS - numGamesRented()) <= storeStock) && (this.numGamesToRent <= storeStock) && ((MAX_RENTALS - numGamesRented()) >= this.numGamesToRent);
+            // Returns the number of games that the customer currently has rented.
+
+            int count = 0;
+            foreach (IRental r in this.rentals)
+            {
+                foreach (IGame g in r.GetGames())
+                {
+                    count++;
+                }
+            }
+            return count;
         }
+
     }
 }
--- ICustomer.cs
+++ ICustomer.cs
@@ -5,11 +5,12 @@ namespace Homework1_Entertainment_Store
     public interface ICustomer
     {
         public string GetName(); //returns customer name
+        public bool CanRent(int storeStock); //given a stores stock, returns whether or not customer can rent
         public void RollBehavior(); //updates random behavior for renting
         public IRental CreateRental(int day); //creates a rental request with all needed info, to be populated by store
         public double PayFor(IRental r); //adds the given rental to rented games and returns payment
         List<IRental> ReturnGames(int day); //populates a list with rentals that need to be returned on the given day.
-        public bool CanRent(int storeStock); //given a stores stock, returns whether or not customer can rent
+
     }
 
 
